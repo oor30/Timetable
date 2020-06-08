7b0f316d7ff6bd083e24cab007d2d05f4f4cb99c
Kazuki Wakamatsu <x3033171@edu.gifu-u.ac.jp>
2020-06-08 23:13:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/x3033171/timetable/Lecture.java
@@ -0,0 +1,42 @@
+package com.example.x3033171.timetable;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class Lecture extends ConstraintLayout implements View.OnClickListener {
+    TextView lecName, lecRoom;
+
+    public Lecture(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public Lecture(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public Lecture(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        View layout = LayoutInflater.from(context).inflate(R.layout.lecture_layout, this);
+        lecName = (TextView)layout.findViewById(R.id.lecName);
+        lecRoom = (TextView)layout.findViewById(R.id.lecRoom);
+        layout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.lecLayout) {
+
+        }
+    }
+}
