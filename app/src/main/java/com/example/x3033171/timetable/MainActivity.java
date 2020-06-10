630fb650fb6631fdcab8a49c3aa3a0ca7efbc11c
Kazuki Wakamatsu <x3033171@edu.gifu-u.ac.jp>
2020-06-10 12:21:26 +0900

    Firebase Cloud Firestore を導入

--- app/src/main/java/com/example/x3033171/timetable/MainActivity.java
+++ app/src/main/java/com/example/x3033171/timetable/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.x3033171.timetable;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+
+//        searchLecture("semester", "前学期");
+
+        db.collectionGroup("timeroom").whereEqualTo("semester", "前学期").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot snap : queryDocumentSnapshots) {
+                            Log.d("TAG", snap.getId() + " => " + snap.getData());
+                        }
+                    }
+                });
+
+    }
+
+    void searchLecture (String field, String value) {
+        db.collection("lectures")
+                .whereEqualTo(field, value)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + "=>" +document.getData());
+                            }
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
